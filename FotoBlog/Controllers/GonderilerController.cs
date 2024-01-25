6cab13080bfdf2d73d759d7188c4232a20f1c278
Alican Dökmen <alican@outlook.com>
2024-01-25 03:20:26 +0300

    güncelleme ve silme eklendi

--- FotoBlog/Controllers/GonderilerController.cs
+++ FotoBlog/Controllers/GonderilerController.cs
@@ -51,5 +51,87 @@ namespace FotoBlog.Controllers
 
             return View();
         }
+
+
+        public IActionResult Guncelle(int id)
+        {
+
+            var guncellenecekPost = _db.Gonderiler.Find(id);
+
+            YeniGonderiViewModel guncellenecekModel = new YeniGonderiViewModel();
+            guncellenecekModel.Baslik = guncellenecekPost!.Baslik;
+
+            TempData["Id"] = guncellenecekPost.Id;
+
+            return View(guncellenecekModel);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Guncelle(YeniGonderiViewModel vm)
+        {
+            var guncellenecekPost = _db.Gonderiler.Find(TempData["Id"])!;
+
+            try
+            {
+                if (vm.Resim != null && vm.Resim.FileName != guncellenecekPost.ResimYolu)
+                {
+                    ResimSil(guncellenecekPost);
+
+                    string dosyaAd = vm.Resim.FileName; //dosya adını al
+                    string ext = Path.GetExtension(dosyaAd); //uzantı al
+                    string yeniDosyaAd = Guid.NewGuid() + ext;
+                    string yol = Path.Combine(_env.WebRootPath, "img", "upload", yeniDosyaAd);
+
+                    using (var fs = new FileStream(yol, FileMode.CreateNew))
+                    {
+                        vm.Resim.CopyTo(fs);
+                    }
+
+                    guncellenecekPost.ResimYolu = yeniDosyaAd;
+                }
+
+                guncellenecekPost.Baslik = vm.Baslik;
+                _db.Gonderiler.Update(guncellenecekPost);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["Durum"] = "Hata oluştu! " + ex.Message;
+                return View();
+            }
+
+        }
+
+
+        public IActionResult Sil(int id)
+        {
+            Gonderi post = _db.Gonderiler.Find(id)!;
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult Sil(Gonderi post)
+        {
+            ResimSil(post);
+            _db.Gonderiler.Remove(post);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public void ResimSil(Gonderi gonderi)
+        {
+            var resmiKullananBaskaVarMi = _db.Gonderiler.Any(g => g.ResimYolu == gonderi.ResimYolu && g.Id != gonderi.Id);
+
+            if (gonderi.ResimYolu != null && !resmiKullananBaskaVarMi)
+            {
+                string dosya = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/upload", gonderi.ResimYolu);
+                System.IO.File.Delete(dosya);
+            }
+
+        }
+
     }
 }
